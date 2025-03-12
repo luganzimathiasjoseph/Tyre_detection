32ada8d6353b0c8d43bd8309dd2d5433e376603a
Luganzi Mathias Joseph <mathiasjoseph250@gmail.com>
2025-03-12 16:53:16 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     img_bytes = io.BytesIO()
     image.save(img_bytes, format="JPEG")
