1010c6019341ef4ee59b63df4d756ce0e7710fce
Luganzi Mathias Joseph <mathiasjoseph250@gmail.com>
2025-04-14 16:47:32 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,28 @@
-import streamlit as st
-import requests
-from PIL import Image
-import io
+# import streamlit as st
+# import requests
+# from PIL import Image
+# import io
 
-# Flask API endpoint
-FLASK_API_URL = "https://liammatt5-tyre-detection.hf.space/predict"
+# # Flask API endpoint
+# FLASK_API_URL = "https://liammatt5-tyre-detection.hf.space/predict"
 
-st.title("Tyre Defect Detection")
-st.write("Upload an image of a tyre.")
+# # st.title("Tyre Defect Detection")
+# st.write("Upload an image of a tyre.")
 
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+# uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_container_width=True)
+# if uploaded_file is not None:
+#     image = Image.open(uploaded_file)
+#     st.image(image, caption="Uploaded Image", use_container_width=True)
 
-    img_bytes = io.BytesIO()
-    image.save(img_bytes, format="JPEG")
+#     img_bytes = io.BytesIO()
+#     image.save(img_bytes, format="JPEG")
 
-    if st.button("Predict"):
-        response = requests.post(FLASK_API_URL, files={"file": img_bytes.getvalue()})
+#     if st.button("Predict"):
+#         response = requests.post(FLASK_API_URL, files={"file": img_bytes.getvalue()})
         
-        if response.status_code == 200:
-            result = response.json().get("result", "Unknown")
-            st.success(f"Prediction: {result}")
-        else:
-            st.error(f"Error: {response.json().get('error', 'Something went wrong')}")
+#         if response.status_code == 200:
+#             result = response.json().get("result", "Unknown")
+#             st.success(f"Prediction: {result}")
+#         else:
+#             st.error(f"Error: {response.json().get('error', 'Something went wrong')}")
